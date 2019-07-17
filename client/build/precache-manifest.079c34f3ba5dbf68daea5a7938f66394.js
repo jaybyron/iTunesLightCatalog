b2aa017cbf41d0dc4fa77c78093a92a30799ce10
Byron Inocencio <jay.byron@gmail.com>
2019-07-17 09:19:28 -0700

    added build files

---
+++ client/build/precache-manifest.079c34f3ba5dbf68daea5a7938f66394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b974dffb10be330b613539e571df94c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "106f8c0952c99cd9ca7d",
+    "url": "/static/css/main.f80e4630.chunk.css"
+  },
+  {
+    "revision": "4fa9435ee42090a413ff",
+    "url": "/static/js/2.202f4d6a.chunk.js"
+  },
+  {
+    "revision": "106f8c0952c99cd9ca7d",
+    "url": "/static/js/main.2267e62b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
